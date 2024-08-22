ff96a6aec199ff7554ae19ef4f32ddaabce48225
Heriberto Santana <heriberto.santana@dci-student.org>
2024-08-22 09:29:15 +0200

    exercises

--- src/pages/Exercises/index.jsx
+++ src/pages/Exercises/index.jsx
@@ -1,23 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { RenderCode } from "../../components/RenderCode";
 import { fetchData } from "../../hooks/fetchData";
 import styles from "./Exercise.module.css";
 
-
-
 export function Exercises() {
   const { data, loading, error } = fetchData("/Exercises.json");
+  const [showPopover, setShowPopover] = useState(false);
+  const [popoverContent, setPopoverContent] = useState(null);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error}</p>;
-  // <RenderCode code={element.requirements} language="md"/>
+
+  function handleClick(element) {
+    setPopoverContent(element); 
+    setShowPopover(true); 
+  }
+
+  function handlePopover() {
+    setShowPopover(false);
+  }
 
   return (
     <>
       <header className={styles.header}>
         <h1 className={styles.h1}>Practice Exercises</h1>
         <div>
-          <h2 className={styles.h2}>Sharpen Your React Skills with Tailored Exercises</h2>
+          <h2 className={styles.h2}>
+            Sharpen Your React Skills with Tailored Exercises
+          </h2>
           <p className={styles.p}>
             On this page, you’ll find a collection of exercises categorized into
             three levels: Beginner, Intermediate, and Advanced. Each exercise is
@@ -31,20 +41,34 @@ export function Exercises() {
       </header>
       <main className={styles.main}>
         <ul className={styles.ul}>
-          {data.map(element => {
-            return(
-              <li
-              className={styles.li} 
-              key={element.title}>
+          {data.map((element) => {
+            return (
+              <li className={styles.li} key={element.title}>
                 <p className={styles.li_p}>{element.level}</p>
                 <h2 className={styles.h2}>{element.title}</h2>
                 <p className={styles.p}>{element.objective}</p>
+                <button className={styles.button} onClick={() => handleClick(element)}>More info</button>
               </li>
-            )
+            );
           })}
-          
         </ul>
+        
       </main>
+     
+      {showPopover && (
+         <aside className={styles.aside}>
+          <div className={styles.popover}>
+            <button onClick={handlePopover} className={styles.button}>
+              X
+            </button>
+            <p className={styles.li_p}>{popoverContent.level}</p>
+            <h3>{popoverContent.title}</h3>
+            <p className={styles.p}>{popoverContent.description}</p>
+            <RenderCode code={popoverContent.requirements} language="md" />
+          </div>
+      </aside>
+
+        )}
     </>
   );
 }
